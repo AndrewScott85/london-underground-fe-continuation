da92862d56b718259d3ee6096f89facab9f4b1ae
alech <hamiltonalec@gmail.com>
2022-06-20 16:29:34 +0100

    expand-collapse added not working

--- src/Components/Journeys/AvailableJourneys/index.js
+++ src/Components/Journeys/AvailableJourneys/index.js
@@ -1,27 +1,40 @@
+import StationList from "../StationList";
+import {useState} from "react";
+import {MdExpandMore} from 'react-icons/md';
+import {MdExpandLess} from 'react-icons/md';
+
 const AvailableJourneys = ({journeyOptions}) => {
+
+    const [open, setOpen] = useState(false);
+
+    const collapseIcon = <MdExpandMore className="expand-collapse" size="40px" onClick={() => setOpen(!open)} />
+    const expandIcon = <MdExpandLess className="expand-collapse" size="40px" onClick={() => setOpen(!open)} />
+
     return (
         <>
         <h2>Available Routes</h2>
         <table>
             <thead>
-            <tr>
-                <th style={{width: "50%"}}>Line</th>
-                <th>Journey Time</th>
-                <th>Stops</th>
-            </tr>
+                <tr>
+                    <th style={{width: "50%"}}>Line</th>
+                    <th>Journey Time</th>
+                    <th>Stops</th>
+                </tr>
             </thead>
             {journeyOptions.map((item, index) => {
                 return (
                     <tbody key={index}>
-                    <tr>
-                        <td>{item.line}</td>
-                        <td>69m</td>
-                        <td>{item.stops}</td>
-                    </tr>
+                        <tr>
+                            <td onClick={() => setOpen(!open)}>{item.line}{open ? collapseIcon : expandIcon}</td>
+                            <td>69m</td>
+                            <td>{item.stops}</td>
+                        </tr>
                     </tbody>
                 );
             })}
         </table>
+        {open ? collapseIcon : expandIcon}
+        {open && <StationList />}
         </>
     );
 }
---
+++ src/Components/Journeys/StationList/index.js
@@ -0,0 +1,28 @@
+const StationList = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Station</th>
+                    <th>Time to next</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Bond Street</td>
+                    <td>30m</td>
+                </tr>
+                <tr>
+                    <td>Bond Street</td>
+                    <td>30m</td>
+                </tr>
+                <tr>
+                    <td>Bond Street</td>
+                    <td>30m</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default StationList;
