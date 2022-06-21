44fb73bde9c64838b07d7c1f3f73f878d06d5667
alech <hamiltonalec@gmail.com>
2022-06-21 16:14:22 +0100

    cleaned up some code

--- src/Components/Journeys/AvailableJourneys/index.js
+++ src/Components/Journeys/AvailableJourneys/index.js
@@ -8,6 +8,7 @@ const AvailableJourneys = ({journeyOptions}) => {
         <h2>Available Routes</h2>
         <div className="journey-table">
             <h3>Line</h3>
+            <h3>Changes</h3>
             <h3>Length</h3>
             <h3>Stops</h3>
             {journeyOptions.map((item, index) => {
--- src/Components/Journeys/StationList/index.js
+++ src/Components/Journeys/StationList/index.js
@@ -43,7 +43,10 @@ const StationList = ({item}) => {
 
     return (
         <>
-        <div className="line-img-container"><img src={displaySvg(item.line)} onClick={() => setVisible(!visible)} alt="" />{visible ? expandIcon : collapseIcon}</div>
+        <div className="line-img-container">
+            <img src={displaySvg(item.line)} onClick={() => setVisible(!visible)} alt="" />{visible ? expandIcon : collapseIcon}
+        </div>
+        <p>0</p>
         <p>{secondsToHms(item.time)}</p>
         <p>{item.stops}</p>
             {visible && <table className="station-list-table" cellSpacing="0" cellPadding="0">
